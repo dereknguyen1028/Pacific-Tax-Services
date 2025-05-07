3b843d21a0326120565e1c69e1d0a6601b5e5d30
Eltik <xp10d363@gmail.com>
2025-05-06 22:26:46 -0700

    feat: Fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,12 +53,7 @@ export default {
                     "2": "hsl(var(--chart-2))",
                     "3": "hsl(var(--chart-3))",
                     "4": "hsl(var(--chart-4))",
-                    "5": "hsl(var(--chart-5))",
-                    "1": "hsl(var(--chart-1))",
-                    "2": "hsl(var(--chart-2))",
-                    "3": "hsl(var(--chart-3))",
-                    "4": "hsl(var(--chart-4))",
-                    "5": "hsl(var(--chart-5))",
+                    "5": "hsl(var(--chart-5))"
                 },
                 sidebar: {
                     DEFAULT: "hsl(var(--sidebar-background))",
